ed0b7ee664b240ce3aa195bc321e47b1c8f21226
EdoardoBaral <edoardo@baral.it>
2025-11-15 23:31:23 +0100

    Creazione del progetto BetaShopUdemy

---
+++ src/main/java/it/udemy/BetaShopApplication.java
@@ -0,0 +1,12 @@
+package it.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaShopApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(BetaShopApplication.class, args);
+	}
+}
